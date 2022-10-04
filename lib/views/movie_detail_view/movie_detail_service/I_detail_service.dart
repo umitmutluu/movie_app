a8bcaf8b1804dc2b24786641144d31ebf61e2a31
umitmutluu <umitt.mmutlu@gmail.com>
2022-10-04 21:36:10 +0300

    Detail | service builded

--- lib/views/movie_detail_view/movie_detail_service/I_detail_service.dart
+++ lib/views/movie_detail_view/movie_detail_service/I_detail_service.dart
@@ -1,26 +1,26 @@
 import 'package:movie_app/core/constants/application_constant.dart';
-import 'package:movie_app/models/movie_detail_model/movie_detail_model.dart';
+import 'package:movie_app/product/models/movie_detail_model/movie_detail_model.dart';
 import 'package:vexana/vexana.dart';
 
 abstract class IDetailService {
   final INetworkManager networkManager;
 
   IDetailService({required this.networkManager});
-  final String detailPath = IDetailServicePath.DETAIL.returnValue;
-  final String baseUrl = IDetailServicePath.BASEURL.returnValue;
+  final String detailPath = IDetailServicePath.detail.returnValue;
+  final String baseUrl = IDetailServicePath.baseUrl.returnValue;
   final String apiKey = ApplicationConstants.instance.apiKey;
   Future<MovieDetailModel?> fetchMoveDetail({required String movieId});
 }
-enum IDetailServicePath { BASEURL,DETAIL }
+enum IDetailServicePath { baseUrl,detail }
 
 
 extension IWordServicePathExtension on IDetailServicePath {
   String get returnValue {
     switch (this) {
-      case IDetailServicePath.DETAIL:
+      case IDetailServicePath.detail:
         return "movie/";
-      case IDetailServicePath.BASEURL:
-        return ApplicationConstants.instance.BASE_URL;
+      case IDetailServicePath.baseUrl:
+        return ApplicationConstants.instance.baseUrl;
       default:
         return "";
     }
--- lib/views/movie_detail_view/movie_detail_service/detail_service.dart
+++ lib/views/movie_detail_view/movie_detail_service/detail_service.dart
@@ -1,4 +1,4 @@
-import 'package:movie_app/models/movie_detail_model/movie_detail_model.dart';
+import 'package:movie_app/product/models/movie_detail_model/movie_detail_model.dart';
 import 'package:movie_app/views/movie_detail_view/movie_detail_service/I_detail_service.dart';
 import 'package:vexana/vexana.dart';
 
@@ -9,7 +9,7 @@ class DetailService extends IDetailService {
   @override
   Future<MovieDetailModel?> fetchMoveDetail({required String movieId})async {
     final response = await networkManager
-        .send<MovieDetailModel, MovieDetailModel>(baseUrl + detailPath +movieId +apiKey,
+        .send<MovieDetailModel, MovieDetailModel>( detailPath +movieId +apiKey,
         parseModel: MovieDetailModel(),
         method: RequestType.GET);
 final result = response.data;
